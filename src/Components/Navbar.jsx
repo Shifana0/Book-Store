a6ef4f2cfeb1489425e372e8251afc3d043929b2
Fathima Shifana <shifana.4727@gmail.comgit config --global user.name>
2025-03-28 00:23:31 +0530

    book hall

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
         <h1 className='ml-10 text-2xl font-bold'>Books-Store</h1>
 
         <ul className='flex gap-10 font-bold '>
-           <Link to={'/'}><li>Home</li></Link> 
+           <Link to={'/home'}><li>Home</li></Link> 
            <Link to={'/cart'}><li>Cart</li></Link>
            <Link to={'/wishlist'}><li>Wishlist</li></Link>
         </ul>
